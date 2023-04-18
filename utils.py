365fa7cc67462b977869c778d0926867f0d81b35
mjkareem <mujahidakande@gmail.com>
2023-04-18 20:59:14 +0200

    Commiting utils.py for male passengers

--- utils.py
+++ utils.py
@@ -5,7 +5,7 @@ def load_passengers():
     # Load the Titanic dataset
     df = pd.read_csv('data/titanic.csv')
 
-    # Select only the female passengers
-    female_passengers = df[df['Sex'] == 'female']
+    # Select only the male passengers
+    male_passengers = df[df['Sex'] == 'male']
 
-    return female_passengers
+    return male_passengers
